d4768bf37920ab13fa6da86adec8b15a4dbeece2
MURALITHARAN D <131011398+dmmuralitharan@users.noreply.github.com>
2024-08-08 20:24:23 +0530

    mat button toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonToggle, MatButtonToggleGroup } from '@angular/material/button-toggle';
+import { MatDivider } from '@angular/material/divider';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule, MatButtonToggle, MatButtonToggleGroup, MatDivider, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'learn-angular-material';
+
+  hideMultipleSelectionIndicator = signal(true)
+  hideSingleSelectionIndicator = signal(true)
+
+  selectedValuesByForms!: string
+  selectedValuesByReactiveForms = new FormControl('')
 }
