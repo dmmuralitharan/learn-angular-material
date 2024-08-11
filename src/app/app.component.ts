0cd786ab4f877bb5c8148afaadc0e92f1b7d974c
MURALITHARAN D <131011398+dmmuralitharan@users.noreply.github.com>
2024-08-11 11:47:31 +0530

    4-mat-badges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatBadgeModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'learn-angular-material';
+
+  hideMessageBadge = signal<boolean>(false)
+  matBadgeCount = signal<number>(20)
+
+  readMessage(): void {
+    // this.hideMessageBadge.set(true)
+    // this.matBadgeCount.set(0)
+    
+
+
+    this.hideMessageBadge.set(!this.hideMessageBadge())
+  }
 }
