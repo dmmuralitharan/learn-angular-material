c546419b218096fc068e99db3ec196ee42563fdd
MURALITHARAN D <131011398+dmmuralitharan@users.noreply.github.com>
2024-08-14 22:18:45 +0530

    11-mat-grid-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'learn-angular-material';
 }
