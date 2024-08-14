4ca4253e29b8a264fcbe8d9d3b5488a27f8db86e
MURALITHARAN D <131011398+dmmuralitharan@users.noreply.github.com>
2024-08-14 22:04:22 +0530

    10-mat-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { DatePipe } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatListModule, MatIconModule, MatDividerModule, DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'learn-angular-material';
+
+  frontendProgramming: String[] = ['Angular', 'React', 'Vue']
 }
